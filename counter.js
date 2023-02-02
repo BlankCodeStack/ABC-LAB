33f249bae12c9ce6796aa0cc5d60ccef79ecad05
BlankCode <110416295+BlankCodeStack@users.noreply.github.com>
2023-02-02 18:59:07 +0900

    카운터: 연습

---
+++ counter.js
@@ -0,0 +1,17 @@
+const number = document.getElementById("number");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+increase.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current + 1;
+};
+
+decrease.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  if (number.innerText <= 0){
+    current 
+  } else {
+    number.innerText = current -1;
+  }
+};
